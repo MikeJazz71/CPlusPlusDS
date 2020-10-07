01e5fca06bd8e2822275adb50a99fef540767076
Mike Jasiczek <mjjasiczek@mnu.edu>
2020-10-07 17:19:09 -0500

    Queue Start

--- Ch-05/Sources/QueType.cpp
+++ Ch-05/Sources/QueType.cpp
@@ -12,76 +12,35 @@
 
 QueType::QueType(void)
 {
-    frontPtr = rearPtr = NULL;
+    
 }
 QueType::~QueType()
 {
-    MakeEmpty();
+    
 }
 
 //	Transformers.
 void QueType::Enqueue(ItemType newItem)
 {
-    if (IsFull())
-    {
-        throw FullQueue();
-    }
-    NodeType *location = new NodeType;
-    location->info = newItem;
-    location->nextPtr = NULL;
-    if (rearPtr == NULL)
-    {
-        frontPtr = location;
-    }
-    else
-    {
-        rearPtr->nextPtr = location;
-    }
-    rearPtr = location;
+  
 }
 void QueType::Dequeue(void)
 {
-    if (IsEmpty())
-    {
-        throw EmptyQueue();
-    }
-    NodeType *tempPtr = frontPtr;
-    frontPtr = frontPtr->nextPtr;
-    if (frontPtr == NULL)
-    {
-        rearPtr = NULL;
-    }
-    delete tempPtr;
+   
 }
 void QueType::MakeEmpty(void)
 {
-    while (frontPtr != NULL)
-    {
-        NodeType *tempPtr = frontPtr;
-        frontPtr = frontPtr->nextPtr;
-        delete tempPtr;
-    }
-    rearPtr = NULL;
+   
 }
 
 //	Observers.
 bool QueType::IsFull(void) const
 {
-    NodeType *location;
-    try
-    {
-        location = new NodeType;
-    }
-    catch (std::bad_alloc exception)
-    {
-        return true;
-    }
-    delete location;
-    return false;
+    
 }
 bool QueType::IsEmpty(void) const
 {
-    return frontPtr == NULL;
+    
 }
 
 #endif //QUETYPE_CPP
